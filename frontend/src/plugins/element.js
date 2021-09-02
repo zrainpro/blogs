bcfcfbe7939a0691cfaa188d7eea6c4f42884c42
zhangyushuai <zhangyushuai126@126.com>
2021-09-02 16:26:13 +0800

    升级依赖, 启动 gzip 与 br 压缩, 提高加载速度

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import '../element-variables.scss'
+// import '../element-variables.less'
 
 Vue.use(Element)
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,40 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const zlib = require('zlib');
 
 module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://[::1]:7001',
+        // target: 'http://[::1]:7001',
+        target: 'http://zrain.top',
         changeOrigin: true,
         pathRewrite: {
           '^/api/': '/'
         }
+      }
+    }
+  },
+
+  pluginOptions: {
+    compression:{
+      brotli: {
+        filename: '[file].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8
+      },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        exclude: 'index.html',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+        deleteOriginalAssets: false
       }
     }
   },
@@ -25,5 +51,7 @@ module.exports = {
     manifestOptions: {
       background_color: '#4DBA87'
     }
-  }
+  },
+
+  productionSourceMap: false
 }
